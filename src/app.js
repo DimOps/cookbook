f29e9f09992fe4b5a5dc60f79ba0a094d0a06729
dimitar <90458809+dimops@users.noreply.github.com>
2022-11-27 18:48:11 +0200

    Details and edit views with static templates added

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import { decorateContext } from './middlewares/render.js';
 
 import { catalogPage } from './views/catalog.js';
 import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
 import { homePage } from './views/home.js';
 import { loginPage } from './views/login.js';
 import { registerPage } from './views/register.js';
@@ -11,9 +13,12 @@ import { registerPage } from './views/register.js';
 page(decorateContext);
 page('/', homePage);
 page('/catalog', catalogPage);
+page('/details/:id', detailsPage);
 page('/login', loginPage);
 page('/register', registerPage);
 page('/create', createPage);
+page('/edit/:id', editPage);
+
 page.start();
 
 
