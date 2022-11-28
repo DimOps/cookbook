fe7dd9ca3613432757bbf69c4769df1eb331fce3
dimitar <90458809+dimops@users.noreply.github.com>
2022-11-28 16:28:59 +0200

    Register functionality added

--- src/views/register.js
+++ src/views/register.js
@@ -1,10 +1,12 @@
 import { html } from '../bundler.js';
+import { register } from '../api/user.js';
+import { createSubmitHandler } from '../utils.js';
 
-const registerTemplate = () => html`
+const registerTemplate = (onSubmit) => html`
 <section id="register">
     <article>
         <h2>Register</h2>
-        <form id="registerForm">
+        <form @submit=${onSubmit} id="registerForm">
             <label>E-mail: <input type="text" name="email"></label>
             <label>Password: <input type="password" name="password"></label>
             <label>Repeat: <input type="password" name="rePass"></label>
@@ -16,6 +18,25 @@ const registerTemplate = () => html`
 
 export function registerPage(ctx) {
     
-    ctx.render(registerTemplate());
+    ctx.render(registerTemplate(createSubmitHandler(ctx, onSubmit)));
 
+}
+
+async function onSubmit (ctx, data, e) {
+
+    try{
+        if (data.email == '' || data.password == '') {
+
+            throw alert('All fields are required!')        
+        }
+        if (data.password != data.rePass) {
+            throw alert('Passwords don\'t match!')
+        }
+
+        await register(data.email, data.password);
+        e.target.reset();
+        ctx.page.redirect('/catalog');
+    } catch (err) {
+        throw err;
+    }
 }
