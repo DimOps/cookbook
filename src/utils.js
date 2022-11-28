947bc721c28b73f28a8f3a48916192492e98fbec
dimitar <90458809+dimops@users.noreply.github.com>
2022-11-28 13:16:03 +0200

    Submit form handler added

--- src/utils.js
+++ src/utils.js
@@ -11,3 +11,12 @@ export function setUserData(data) {
 export function clearUserData() {
     storage.removeItem('userData');
 }
+
+export function createSubmitHandler (ctx, handler) {
+    return function(e) {
+        e.preventDefault();
+        const formData = Object.fromEntries(new FormData(e.target));
+
+        handler(ctx, formData)
+    };
+}
